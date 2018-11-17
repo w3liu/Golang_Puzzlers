d19440da0861287b8652e2957bfca5c61b126729
w3liu <w3liu@qq.com>
2018-11-18 00:24:39 +0800

    done

---
+++ article1/demo2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var name string
+
+func init() {
+
+}
+
+func main() {
+	fmt.Printf("Hello, %s!\n", name)
+}
---
+++ article1/demo3.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var name string
+
+var cmdLine = flag.NewFlagSet("question", flag.ExitOnError)
+
+//func init()  {
+//	flag.CommandLine = flag.NewFlagSet("", flag.PanicOnError)
+//	flag.CommandLine.Usage = func() {
+//		fmt.Fprintf(os.Stderr, "Usage of %s:\n", "question")
+//		flag.PrintDefaults()
+//	}
+//
+//	flag.StringVar(&name, "name", "everyone", "The greeting object.")
+//}
+
+func init() {
+	cmdLine.StringVar(&name, "name", "everyone", "The greeting object.")
+
+}
+
+func main() {
+	//flag.Usage = func() {
+	//	fmt.Printf("Usage of %s:\n", "question")
+	//	flag.PrintDefaults()
+	//}
+	//flag.Parse()
+	cmdLine.Parse(os.Args[1:])
+	fmt.Printf("Hello, %s!\n", name)
+}
